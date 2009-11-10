3029310e4585ecab4e8fe475a62b6d970ef27588
eshamay <eric.shamay@gmail.com>
2009-11-10 10:59:49 -0800

    Commenting a bit for other's eyes

--- prmtop-file.lisp
+++ prmtop-file.lisp
@@ -29,17 +29,18 @@
           value = data that follows"
   (with-open-file (f filename
 		     :direction :input)
-      (read-line f nil nil)		; trim off the first header line
-      (let ((alist ())
-	    (data ()))
-	(loop for line = (trim-string (read-line f nil nil)) while (not (null line)) do
-	     (cond
-	       ;; for each flag we find we parse the flag followed by the data it represents
-	       ((flag-p line)
-		(acons (car data) (cdr data) alist)
-		(setf data (list (parse-flag line))))
-	       ((format-p line) nil)
-	       ((not (eql data nil))
+      (read-line f nil nil)		; trim off the first header line and skip the header flag
+      (let ((alist ())			; This holds the parsed flag/data from the prmtop file
+	    (data ()))			; While this is used to build up individual entries
+	(loop for line = (trim-string (read-line f nil nil))
+	   while (not (null line)) do
+	     (cond			; 3 possible tasks - parse a flag, a format, or data
+	       ;; Check which task to perform, and build the data
+	       ((flag-p line)		; Parsing flags
+		(acons (car data) (cdr data) alist) ; Push any previous data into the alist
+		(setf data (list (parse-flag line)))) ;... and then begin adding data to the new flag
+	       ((format-p line) nil)	; Skipping formatting strings
+	       ((not (eql data nil))	; Parsing data to be paired with the flag
 		(nconc data (string-to-list line)))))
 	(acons (car data) (cdr data) alist)
 	alist)))
